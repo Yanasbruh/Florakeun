f0fbf8e466592c5ec9be6867b779b16d24ae4eec
Yanasbruh <denadewa9@gmail.com>
2022-06-30 20:26:52 +0800

    revisi

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// Tempat Variabel
 const menuToggle = document.querySelector(".menu-toggle input");
 const nav = document.querySelector("nav ul");
 
+// Tempat Memanggil Variabel pada Mobile Device
 menuToggle.addEventListener("click", function () {
-  nav.classList.toggle("slide");
+  nav.classList.toggle("show");
 });
